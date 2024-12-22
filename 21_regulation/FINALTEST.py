afd0a730e9c61bd20bc12c036e2c86e52f4176b3
vinayavr <10337vinaya.v.r@gmail.com>
2024-12-23 01:02:26 +0530

    Modified CO components CT2 marks for CO2 and CO3, CT3 marks for CO4 and CO5

--- 21_regulation/FINALTEST.py
+++ 21_regulation/FINALTEST.py
@@ -20,6 +20,11 @@ bold_border = styles.Border(left=styles.Side(border_style='thin', color='000000'
                 top=styles.Side(border_style='thin', color='000000'),
                 bottom=styles.Side(border_style='thin', color='000000'))
 
+co2_list = [1,2,3,4,5,6,12,15]
+co3_list = [7,8,9,10,11,13,14,16]
+co4_list = [1,2,3,4,5,6,12,13]
+co5_list = [7,8,9,10,11,14,15,16]
+
 app = Flask(__name__)
 
 
@@ -85,8 +90,6 @@ def process_pdfs_in_folder(file_order, folder_path, reg_numbers):
             if extracted_info[key]:
                 info[key] = extracted_info[key]
     return results, info
-
-def extract_question_numbers_from_pdf(pdf_path, question_numbers, marks):
     """
     Extracts question numbers (Q.no) from a question paper PDF.
 
@@ -126,6 +129,57 @@ def extract_question_numbers_from_pdf(pdf_path, question_numbers, marks):
                             except ValueError:
                                 pass  # Skip if the match isn't a valid integer
 
+def extract_question_numbers_from_pdf(pdf_path, question_numbers,marks, co_lists):
+    """
+    Extracts question numbers (Q.no) from a question paper PDF.
+
+    Args:
+        pdf_path (str): Path to the PDF file.
+
+    Returns:
+        list: A list of extracted question numbers as integers.
+    """
+
+    # Define a regex pattern to match question numbers (e.g., "1", "2", etc.)
+    flag=0
+
+    with pdfplumber.open(pdf_path) as pdf:
+        for page_num, page in enumerate(pdf.pages):
+            # Extract all text from the page
+            text = page.extract_text()
+
+            if text: 
+                text = text.replace('&', '"&"')
+                 # Ensure the page has text
+                # Split the text into lines
+                lines = text.split("\n")
+
+                for line in lines:
+                    if flag==0 and line.find("Part")!=-1:
+                        flag=1
+                        continue
+                    if flag==1:
+                        match = re.search(r".\d \d \d \d$",line.strip()) 
+                        question_no= re.match(r"^(\d{2})|\d",line.strip()) 
+                        if question_no and match:
+                            # Add the matched number to the list
+                            try:
+                                qnum = int(question_no.group())
+                                if co_lists == []:
+                                    question_numbers[0].append(int(question_no.group()))
+                                    marks[0].append(int(match.group().strip().split()[0]))
+                                else:
+                                    if qnum in co_lists[0]:
+                                        question_numbers[0].append(int(question_no.group()))
+                                        marks[0].append(int(match.group().strip().split()[0]))
+                                    if qnum in co_lists[1]:
+                                        question_numbers[1].append(int(question_no.group()))
+                                        marks[1].append(int(match.group().strip().split()[0]))
+
+                            except ValueError:
+                                pass  # Skip if the match isn't a valid integer
+
+
 def generate_question_number(question_numbers,length):
     next=0
     flag=0
@@ -145,7 +199,7 @@ def generate_question_number(question_numbers,length):
             qnum.append("Q"+str(question_numbers[i]))
     return qnum
 
-def generate_first_row(worksheet,co1,co2,co3):
+def generate_first_row(worksheet,co1,co2,co3,co4,co5):
     worksheet.merge_cells(start_row=1, start_column=1, end_row=1, end_column=3)
     worksheet.cell(row=1, column=1).value = "CLAT->"
     worksheet.cell(row=1, column=1).alignment = styles.Alignment(horizontal='center', vertical='center')
@@ -162,11 +216,21 @@ def generate_first_row(worksheet,co1,co2,co3):
     worksheet.cell(row=1, column=4+co1).fill=light_green_fill
 
     worksheet.merge_cells(start_row=1, start_column=4+co1+co2, end_row=1, end_column=3+co1+co2+co3)
-    worksheet.cell(row=1, column=4+co1+co2).value='FT-III'
+    worksheet.cell(row=1, column=4+co1+co2).value='FT-II , FT-IV'
     worksheet.cell(row=1, column=4+co1+co2).alignment = styles.Alignment(horizontal='center', vertical='center')
     worksheet.cell(row=1, column=4+co1+co2).fill=light_green_fill    
 
-def generate_second_row(worksheet,co1,co2,co3):
+    worksheet.merge_cells(start_row=1, start_column=4+co1+co2+co3, end_row=1, end_column=3+co1+co2+co3+co4)
+    worksheet.cell(row=1, column=4+co1+co2+co3).value='FT-III'
+    worksheet.cell(row=1, column=4+co1+co2+co3).alignment = styles.Alignment(horizontal='center', vertical='center')
+    worksheet.cell(row=1, column=4+co1+co2+co3).fill=light_green_fill    
+
+    worksheet.merge_cells(start_row=1, start_column=4+co1+co2+co3+co4, end_row=1, end_column=3+co1+co2+co3+co4+co5)
+    worksheet.cell(row=1, column=4+co1+co2+co3+co4).value='FT-III, REPORT'
+    worksheet.cell(row=1, column=4+co1+co2+co3+co4).alignment = styles.Alignment(horizontal='center', vertical='center')
+    worksheet.cell(row=1, column=4+co1+co2+co3+co4).fill=light_green_fill    
+
+def generate_second_row(worksheet,co1,co2,co3,co4,co5):
     worksheet.merge_cells(start_row=2, start_column=1, end_row=2, end_column=3)
     worksheet.cell(row=2, column=1).value = "CO ->"
     worksheet.cell(row=2, column=1).alignment = styles.Alignment(horizontal='center', vertical='center')
@@ -187,20 +251,39 @@ def generate_second_row(worksheet,co1,co2,co3):
     worksheet.cell(row=2, column=4+co1+co2).alignment = styles.Alignment(horizontal='center', vertical='center')
     worksheet.cell(row=2, column=4+co1+co2).fill=orange_fill
 
-def generate_third_row(worksheet,co1,co2,co3):
+    worksheet.merge_cells(start_row=2, start_column=4+co1+co2+co3, end_row=2, end_column=3+co1+co2+co3+co4)
+    worksheet.cell(row=2, column=4+co1+co2+co3).value='CO4'
+    worksheet.cell(row=2, column=4+co1+co2+co3).alignment = styles.Alignment(horizontal='center', vertical='center')
+    worksheet.cell(row=2, column=4+co1+co2+co3).fill=orange_fill
+
+    worksheet.merge_cells(start_row=2, start_column=4+co1+co2+co3+co4, end_row=2, end_column=3+co1+co2+co3+co4+co5)
+    worksheet.cell(row=2, column=4+co1+co2+co3+co4).value='CO5'
+    worksheet.cell(row=2, column=4+co1+co2+co3+co4).alignment = styles.Alignment(horizontal='center', vertical='center')
+    worksheet.cell(row=2, column=4+co1+co2+co3+co4).fill=orange_fill
+
+def generate_third_row(worksheet,co1,co2,co3,co4,co5):
     worksheet.merge_cells(start_row=3, start_column=4, end_row=3, end_column=3+co1)
-    worksheet.cell(row=3, column=4).value = 'THEORY \n (for either/or Q, \n award marks for the attempted students only)'
+    worksheet.cell(row=3, column=4).value = 'THEORY (for either/or Q, award marks for the attempted students only)'
     worksheet.cell(row=3, column=4).alignment = styles.Alignment(horizontal='center', vertical='center')
 
     worksheet.merge_cells(start_row=3, start_column=4+co1, end_row=3, end_column=3+co1+co2)
-    worksheet.cell(row=3, column=4+co1).value='THEORY \n (for either/or Q, \n award marks for the attempted students only)'
+    worksheet.cell(row=3, column=4+co1).value='THEORY (for either/or Q, award marks for the attempted students only)'
     worksheet.cell(row=3, column=4+co1).alignment = styles.Alignment(horizontal='center', vertical='center')
 
     worksheet.merge_cells(start_row=3, start_column=4+co1+co2, end_row=3, end_column=3+co1+co2+co3)
-    worksheet.cell(row=3, column=4+co1+co2).value='THEORY \n (for either/or Q, \n award marks for the attempted students only)'
+    worksheet.cell(row=3, column=4+co1+co2).value='THEORY (for either/or Q, award marks for the attempted students only)'
     worksheet.cell(row=3, column=4+co1+co2).alignment = styles.Alignment(horizontal='center', vertical='center')
 
-def generate_fourth_row(worksheet,marks1,marks2,marks3,co1,co2,co3):
+    worksheet.merge_cells(start_row=3, start_column=4+co1+co2+co3, end_row=3, end_column=3+co1+co2+co3+co4)
+    worksheet.cell(row=3, column=4+co1+co2+co3).value='THEORY (for either/or Q, award marks for the attempted students only)'
+    worksheet.cell(row=3, column=4+co1+co2+co3).alignment = styles.Alignment(horizontal='center', vertical='center')
+
+    worksheet.merge_cells(start_row=3, start_column=4+co1+co2+co3+co4, end_row=3, end_column=3+co1+co2+co3+co4+co5)
+    worksheet.cell(row=3, column=4+co1+co2+co3+co4).value='THEORY (for either/or Q, award marks for the attempted students only)'
+    worksheet.cell(row=3, column=4+co1+co2+co3+co4).alignment = styles.Alignment(horizontal='center', vertical='center')
+
+
+def generate_fourth_row(worksheet,marks1,marks2,marks3,marks4,marks5,co1,co2,co3,co4,co5):
     worksheet.merge_cells(start_row=4, start_column=1, end_row=4, end_column=3)
     worksheet.cell(row=4, column=1).value = "MAX. MARKS (If not applicable, leave BLANK)->"
     worksheet.cell(row=4, column=1).alignment = styles.Alignment(horizontal='right', vertical='center')
@@ -221,7 +304,17 @@ def generate_fourth_row(worksheet,marks1,marks2,marks3,co1,co2,co3):
         worksheet.cell(row=4, column=4+co1+co2+i).value = marks3[i]
         worksheet.cell(row=4, column=4+co1+co2+i).alignment = styles.Alignment(horizontal='center', vertical='center')
 
-def generate_fifth_row(worksheet,co1,co2,co3):
+    for i in range(0, co4):
+        worksheet.column_dimensions[get_column_letter(4+co1+co2+co3+i)].width=8
+        worksheet.cell(row=4, column=4+co1+co2+co3+i).value = marks4[i]
+        worksheet.cell(row=4, column=4+co1+co2+co3+i).alignment = styles.Alignment(horizontal='center', vertical='center')
+
+    for i in range(0, co5):
+        worksheet.column_dimensions[get_column_letter(4+co1+co2+co3+co4+i)].width=8
+        worksheet.cell(row=4, column=4+co1+co2+co3+co4+i).value = marks5[i]
+        worksheet.cell(row=4, column=4+co1+co2+co3+co4+i).alignment = styles.Alignment(horizontal='center', vertical='center')
+
+def generate_fifth_row(worksheet,co1,co2,co3,co4,co5):
     worksheet.merge_cells(start_row=5, start_column=4, end_row=5, end_column=3+co1)
     worksheet.cell(row=5, column=4).value = 'Question numbers mapping'
     worksheet.cell(row=5, column=4).alignment = styles.Alignment(horizontal='center', vertical='center')
@@ -237,7 +330,18 @@ def generate_fifth_row(worksheet,co1,co2,co3):
     worksheet.cell(row=5, column=4+co1+co2).alignment = styles.Alignment(horizontal='center', vertical='center')
     worksheet.cell(row=5, column=4+co1+co2).fill = grey_fill
 
-def generate_sixth_row(worksheet,question_numbers1,question_numbers2,question_numbers3,co1,co2,co3):
+    worksheet.merge_cells(start_row=5, start_column=4+co1+co2+co3, end_row=5, end_column=3+co1+co2+co3+co4)
+    worksheet.cell(row=5, column=4+co1+co2+co3).value = 'Question numbers mapping'
+    worksheet.cell(row=5, column=4+co1+co2+co3).alignment = styles.Alignment(horizontal='center', vertical='center')
+    worksheet.cell(row=5, column=4+co1+co2+co3).fill = grey_fill
+
+    worksheet.merge_cells(start_row=5, start_column=4+co1+co2+co3+co4, end_row=5, end_column=3+co1+co2+co3+co4+co5)
+    worksheet.cell(row=5, column=4+co1+co2+co3+co4).value = 'Question numbers mapping'
+    worksheet.cell(row=5, column=4+co1+co2+co3+co4).alignment = styles.Alignment(horizontal='center', vertical='center')
+    worksheet.cell(row=5, column=4+co1+co2+co3+co4).fill = grey_fill
+
+
+def generate_sixth_row(worksheet,question_numbers1,question_numbers2,question_numbers3,question_numbers4,question_numbers5,co1,co2,co3,co4,co5):
     worksheet.cell(row=6,column=1).value="Sl.No"
     worksheet.column_dimensions[get_column_letter(1)].width=6
     worksheet.cell(row=6, column=1).alignment = styles.Alignment(horizontal='center', vertical='center')
@@ -265,18 +369,42 @@ def generate_sixth_row(worksheet,question_numbers1,question_numbers2,question_nu
         worksheet.cell(row=6, column=4+co1+co2+i).value =qnum[i]
         worksheet.cell(row=6, column=4+co1+co2+i).alignment = styles.Alignment(horizontal='center', vertical='center')
 
+    qnum=generate_question_number(question_numbers4,co4)
+    for i in range(0, co4):
+        worksheet.cell(row=6, column=4+co1+co2+co3+i).value =qnum[i]
+        worksheet.cell(row=6, column=4+co1+co2+co3+i).alignment = styles.Alignment(horizontal='center', vertical='center')
+
+    qnum=generate_question_number(question_numbers5,co5)
+    for i in range(0, co5):
+        worksheet.cell(row=6, column=4+co1+co2+co3+co4+i).value =qnum[i]
+        worksheet.cell(row=6, column=4+co1+co2+co3+co4+i).alignment = styles.Alignment(horizontal='center', vertical='center')
+
 def generate_excel(pdf_paths):
 
     question_numbers1=[]
-    question_numbers2=[]
-    question_numbers3=[]
     marks1=[]
-    marks2=[] 
-    marks3=[]
+    question_numbers=[[],[]]
+    marks=[[],[]]
 
-    extract_question_numbers_from_pdf(pdf_paths[0], question_numbers1, marks1)
-    extract_question_numbers_from_pdf(pdf_paths[1], question_numbers2, marks2)
-    extract_question_numbers_from_pdf(pdf_paths[2], question_numbers3, marks3)
+    co_lists=[co2_list,co3_list]
+
+    extract_question_numbers_from_pdf(pdf_paths[0], question_numbers, marks, [])
+    question_numbers1=question_numbers[0]
+    marks1=marks[0]
+    question_numbers=[[],[]]
+    marks=[[],[]]
+    extract_question_numbers_from_pdf(pdf_paths[1], question_numbers, marks, co_lists)
+    question_numbers2=question_numbers[0]
+    question_numbers3=question_numbers[1]
+    marks2=marks[0]
+    marks3=marks[1]
+    question_numbers=[[],[]]
+    marks=[[],[]]
+    extract_question_numbers_from_pdf(pdf_paths[2], question_numbers, marks, [co4_list, co5_list])
+    question_numbers4=question_numbers[0]
+    question_numbers5=question_numbers[1]
+    marks4=marks[0]
+    marks5=marks[1]
 
     # Create a new workbook
     workbook = Workbook()
@@ -285,12 +413,15 @@ def generate_excel(pdf_paths):
     co1=len(question_numbers1)
     co2=len(question_numbers2)
     co3=len(question_numbers3)
-    generate_first_row(worksheet,co1,co2,co3)
-    generate_second_row(worksheet,co1,co2,co3)
-    generate_third_row(worksheet,co1,co2,co3)
-    generate_fourth_row(worksheet,marks1,marks2,marks3,co1,co2,co3)
-    generate_fifth_row(worksheet,co1,co2,co3)
-    generate_sixth_row(worksheet,question_numbers1,question_numbers2,question_numbers3,co1,co2,co3)
+    co4=len(question_numbers4)
+    co5=len(question_numbers5)
+    
+    generate_first_row(worksheet,co1,co2,co3,co4,co5)
+    generate_second_row(worksheet,co1,co2,co3,co4,co5)
+    generate_third_row(worksheet,co1,co2,co3,co4,co5)
+    generate_fourth_row(worksheet,marks1,marks2,marks3,marks4,marks5,co1,co2,co3,co4,co5)
+    generate_fifth_row(worksheet,co1,co2,co3,co4,co5)
+    generate_sixth_row(worksheet,question_numbers1,question_numbers2,question_numbers3,question_numbers4,question_numbers5,co1,co2,co3,co4,co5)
     
     times_new_roman_font = Font(name="Times New Roman", size=10, bold=True)
     for row_num, row in enumerate(worksheet.iter_rows(min_row=1, max_row=worksheet.max_row), 1):
@@ -298,8 +429,7 @@ def generate_excel(pdf_paths):
                 cell.font = times_new_roman_font
                 cell.border=bold_border
                 if row_num == 4:
-                    cell.fill = yellow_fill
-            
+                    cell.fill = yellow_fill     
 
 # Adjust the path as needed
     file_path = os.getcwd()+"/output/result.xlsx" 
